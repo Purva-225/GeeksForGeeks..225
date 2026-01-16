ab4928c8f2c96b8227f8879ac31e3e47f4667791
Purva nigade <purvanigade6@gmail.com>
2026-01-17 00:03:59 +0530

    UsingLinearSearch.cpp
    Implement linear search algorithm to find the index of an element in an array.

---
+++ UsingLinearSearch.cpp
@@ -0,0 +1,22 @@
+//Given an array, arr of n integers, and an integer element x, find whether element x is present in the array. 
+//Return the index of the first occurrence of x in the array, or -1 if it doesn't exist.
+//Examples:
+//Input: arr[] = [1, 2, 3, 4], x = 3
+//Output: 2
+class Solution {
+  public:
+    // Function to search x in arr
+    // arr: input array
+    // X: element to be searched for
+    int search(int arr[], int n, int x) {
+
+        // Your code here
+         for(int i = 0; i < n; i++){
+            if(arr[i] == x){
+               return i; 
+            }
+               
+        }
+         return -1;
+    }
+};

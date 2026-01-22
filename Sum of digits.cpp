6294c5877c932f8db0f9e16ae8d4f5fe9ce6aaba
Purva nigade <purvanigade6@gmail.com>
2026-01-22 23:34:06 +0530

    Implement sum of digits function in Solution class

---
+++ Sum of digits.cpp
@@ -0,0 +1,14 @@
+#Sum of digits
+class Solution {
+  public:
+    int sumOfDigits(int n) {
+        // code here
+        int sum = 0;
+        while(n > 0) {
+            sum += n % 10; 
+            n /= 10; 
+        }
+        
+        return sum;
+    }
+};

1acd8fcbd3c6306d3f35b0aefb1dc329628a8c60
Purva nigade <purvanigade6@gmail.com>
2026-01-30 22:41:58 +0530

    Check Dup.cpp

---
+++ Check Dup.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool checkDuplicatesWithinK(vector<int>& arr, int k) {
+     unordered_set<int> st;
+     
+      for(int i=0;i<arr.size();i++){
+          if(st.find(arr[i]) != st.end())
+              return true;        
+              
+              st.insert(arr[i]); 
+              if(i >= k)
+                st.erase(arr[i-k]);
+         }
+         return false;
+    }
+};

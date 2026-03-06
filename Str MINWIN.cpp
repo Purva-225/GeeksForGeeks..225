9421e8ad0b4e904e5e2fc500bd96ae964abb6ecb
Purva nigade <purvanigade6@gmail.com>
2026-03-06 14:47:36 +0530

    minWindow function for substring search

---
+++ Str MINWIN.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  public:
+    string minWindow(string &s, string &p) {
+        
+          vector<int> freq(256, 0);
+
+        for(char c : p)
+            freq[c]++;
+
+        int left = 0, count = p.size();
+        int minLen = INT_MAX, start = 0;
+
+        for(int right = 0; right < s.size(); right++) {
+
+            if(freq[s[right]] > 0)
+                count--;
+
+            freq[s[right]]--;
+
+            while(count == 0) {
+
+                if(right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                freq[s[left]]++;
+                
+                if(freq[s[left]] > 0)
+                    count++;
+
+                left++;
+            }
+        }
+
+        if(minLen == INT_MAX)
+            return "";
+
+        return s.substr(start, minLen);
+    }
+};

06bbf5c9ecf96ba77e1f96e78fb6b69e480194fe
Purva nigade <purvanigade6@gmail.com>
2026-03-23 23:38:16 +0530

    Add URLify function to convert spaces in string
    Implement URLify function to replace spaces with '%20'.

---
+++ URLify Converting space to any other char.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+class Solution {
+  public:
+    string URLify(string& s) {
+        // code here
+      string res = "";
+      
+      for(char c : s){
+          if(c == ' ')
+              res += "%20";
+          else 
+              res += c;
+        }
+        return res;
+    }
+};

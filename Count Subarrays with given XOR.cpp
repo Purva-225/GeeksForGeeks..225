982ca3d875a14762b660d8744b0e09721347923d
Purva nigade <purvanigade6@gmail.com>
2026-04-17 08:45:34 +0530

    Optimize subarray XOR counting algorithm
    Refactored the subarray XOR counting logic for optimization.

--- Count Subarrays with given XOR.cpp
+++ Count Subarrays with given XOR.cpp
@@ -7,14 +7,35 @@ class Solution {
         int cnt = 0; 
        
         for(int i = 0; i < n; i++){
-            int xr = 0;  // moved here
+            int xr = 0;  
             for(int j = i; j < n; j++){
-                xr = xr ^ arr[j];   // accumulate XOR
+                xr = xr ^ arr[j];  
                 if(xr == k){
                     cnt++;
                 }
             }
         }
         return cnt;
+    }
+};
+// optimal
+class Solution {
+  public:
+    long subarrayXor(vector<int> &arr, int k) {
+        int n = arr.size(); 
+        int cnt = 0; 
+         int xr = 0; 
+         
+       map<int,int> mpp; 
+       mpp[xr]++;
+       
+        for(int i =0; i<n; i++){
+              xr = xr^arr[i]; 
+             int x = xr^k; 
+               cnt += mpp[x]; 
+             mpp[xr]++;
+        }
+        return cnt;
+        
     }
 };
